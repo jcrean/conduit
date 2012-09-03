2cb5b47a9a99196634fc3c15168041f735da3cc2
Kyle Burton <kburton@relaynetwork.com>
2012-09-03 12:32:42 -0400

    standarized some of the naming and added tests for basic/gzip conduits

--- src/conduit/core.clj
+++ src/conduit/core.clj
@@ -32,41 +32,18 @@
           ostream
           filters))
 
-(defn pipeline [data-producer data-consumer & stream-filters]
+(defn pipeline [ostream-producer-fn istream-consumer-fn & stream-filters]
   (let [producer-istream (PipedInputStream.)
         producer-ostream (apply-stream-filters
                           (PipedOutputStream. producer-istream)
                           stream-filters)
-        producer-pipe    (pipe-writer producer-ostream data-producer)
-        consumer         (pipe-reader producer-istream data-consumer)]
-    (prn "awaiting producer to complete...")
-    @producer-pipe
-    (prn "producer finished writing to pipeline.")
-    ))
+        producer         (pipe-writer producer-ostream ostream-producer-fn)
+        consumer         (pipe-reader producer-istream istream-consumer-fn)]
+    {:producer producer
+     :consumer consumer}))
 
+(defn basic-conduit [ostream-producer istream-consumer]
+  (pipeline ostream-producer istream-consumer))
 
-(comment
-
-  (defn send-data-into-pipe [^java.io.OutputStream ostream]
-    (prn "sending msg into pipe")
-    (let [msg "hello there"
-          msg-bytes (.getBytes msg "UTF-8")]
-      (.write ostream msg-bytes 0 (count msg-bytes))
-      (prn "done writing")))
-
-
-  (defn collect-data-from-pipe [^java.io.InputStream istream]
-    (prn "collecting message from pipe")
-    (let [bos (java.io.ByteArrayOutputStream.)]
-      (IOUtils/copy istream bos)
-      (prn (str "received message: " (.toString bos "UTF-8")))))
-
-  (defn write-data-to-file [^java.io.InputStream istream]
-    (let [fos (java.io.FileOutputStream. "/tmp/foo.gz")]
-      (IOUtils/copy istream fos)))
-
-  (pipeline send-data-into-pipe collect-data-from-pipe)
-
-  (pipeline send-data-into-pipe write-data-to-file :gzip)
-
-  )
+(defn gzip-conduit [ostream-producer istream-consumer]
+  (pipeline ostream-producer istream-consumer :gzip))
--- test/conduit/test/core.clj
+++ test/conduit/test/core.clj
@@ -2,5 +2,35 @@
   (:use [conduit.core])
   (:use [clojure.test]))
 
-(deftest simple-pipe
-  (is false "test not yet implemented"))
+(defn make-basic-ostream-producer [msg]
+  (let [msg-bytes (.getBytes msg "UTF-8")]
+    (fn [^java.io.OutputStream ostream]
+      (.write ostream msg-bytes 0 (count msg-bytes)))))
+
+(defn make-byte-array-consumer [res]
+  (fn [^java.io.InputStream istream]
+    (let [bos (java.io.ByteArrayOutputStream.)]
+      (org.apache.commons.io.IOUtils/copy istream bos)
+      (reset! res (.toByteArray bos)))))
+
+(deftest test-basic-conduit
+  (let [msg         "this is a test!"
+        collector   (atom (byte-array 0))
+        producer-fn (make-basic-ostream-producer msg)
+        consumer-fn (make-byte-array-consumer collector)
+        conduit     (basic-conduit producer-fn consumer-fn)]
+    @(:consumer conduit)
+    (is (= msg (String. @collector "UTF-8")))))
+
+(deftest test-gzip-conduit
+  (let [msg         "this is a test of gzipping!"
+        collector   (atom (byte-array 0))
+        producer-fn (make-basic-ostream-producer msg)
+        consumer-fn (make-byte-array-consumer collector)
+        conduit     (gzip-conduit producer-fn consumer-fn)
+        unzipper    (fn [bytes]
+                      (let [istream (java.util.zip.GZIPInputStream.
+                                     (java.io.ByteArrayInputStream. bytes))]
+                        (org.apache.commons.io.IOUtils/toByteArray istream)))]
+    @(:consumer conduit)
+    (is (= msg (String. (unzipper @collector) "UTF-8")))))
